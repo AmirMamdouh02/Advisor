d3128af78575c68568f9a4db52b917b9a7824010
AmirMamdouh02 <amirmamdouhcis02@gmail.com>
2023-05-01 15:37:19 +0300

    advisor

---
+++ admin/index.php
@@ -0,0 +1,30 @@
+
+<?php
+include 'allHead.php';
+include './sharedFunc/func.php';
+
+?>
+<main id="main" class="main">
+
+  <div class="pagetitle">
+    <nav>
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+        <li class="breadcrumb-item active"> Home Page</li>
+      </ol>
+    </nav>
+  </div><!-- End Page Title -->
+
+  <section class="section dashboard">
+    <div class="row">
+
+<img src="https://images.pexels.com/photos/414628/pexels-photo-414628.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="">
+    </div><!-- End Right side columns -->
+
+  </section>
+
+</main><!-- End #main -->
+
+<?php
+include 'allUnder.php';
+?>
